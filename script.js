74358049a288a9625c0b1721e4d7dc1b5023b84f
bhagyashri-si <bhagyashri.si.official@gmail.com>
2026-06-09 00:28:07 +0530

    Added JS file

---
+++ script.js
@@ -0,0 +1,30 @@
+
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        const top = window.scrollY;
+        const offset = sec.offsetTop - 150; 
+        const height = sec.offsetHeight;
+        const id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    });
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
